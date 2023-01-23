e04c40dae83fbcbf727cc4273cff16d874e547a4
mdaylight <chunmingws@protonmail.com>
2023-01-23 16:09:52 +0800

    20230123 first version

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+let self = this;
+
+// 搜索功能（预留）
+function searchbutton() {
+    let searchWords = document.querySelector('#index-search-1');
+    let queryWords = searchWords;
+    alert('搜索功能无法使用，但我知道你查询的内容是' + queryWords.value);
+}

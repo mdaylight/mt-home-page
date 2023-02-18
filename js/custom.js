f967402d2341fb2adb773f0013b3960b2ea4bfaf
mdaylight <chunmingws@protonmail.com>
2023-02-18 14:35:50 +0800

    202302181435

--- js/custom.js
+++ js/custom.js
@@ -4,5 +4,9 @@ let self = this;
 function searchbutton() {
     let searchWords = document.querySelector('#index-search-1');
     let queryWords = searchWords;
-    alert('搜索功能目前无法使用，但我知道你查询的内容是 ' + queryWords.value);
+    if (queryWords.value !== "") {
+        alert('搜索功能目前无法使用，但我知道你查询的内容是 ' + queryWords.value);
+    } else {
+        alert('你还没有输入任何内容');
+    }
 }
